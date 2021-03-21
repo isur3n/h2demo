d25b4bb0bd41865106d965b2dfe8d0c0f4e7e4d9
isur3n <surendra.raut@gmail.com>
2021-03-21 10:40:14 -0400

    Initial commit.

---
+++ src/main/java/org/isur3n/app/h2demo/H2demoApplication.java
@@ -0,0 +1,12 @@
+package org.isur3n.app.h2demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class H2demoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(H2demoApplication.class, args);
+	}
+}
---
+++ src/test/java/org/isur3n/app/h2demo/H2demoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.isur3n.app.h2demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class H2demoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
